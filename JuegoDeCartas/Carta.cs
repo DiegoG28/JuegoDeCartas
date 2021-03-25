18c4aa74e292de197d5d4576f28f8ba8d3ca10ca
DIEGO ADOLFO <diegoybaby@hotmail.es>
2021-03-25 00:48:53 -0500

    Versión 1.0: -Variedad de cartas añadidas -Vida añadida a las torres

--- JuegoDeCartas/Carta.cs
+++ JuegoDeCartas/Carta.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace JuegoDeCartas {
-    class Carta {
-        string nombre;
-        int ataque;
-        int defensa;
+    class Carta : Mazo {
+        string type;
+        int power;
+        public int GetPower(int card) {
+            power = int.Parse(deck[1, card]);
+            return power;
+        }
+        public string GetType(int card) {
+            type = deck[14, card];
+            return type;
+        }
     }
 }
